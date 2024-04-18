14da341f919b28bc2da213aaa7e85d2f6b7bd606
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2024-04-18 15:14:04 +0000

    always build, no caching

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-podman build -f=Dockerfile.goscanner -t goscanner .
+podman build -f=Dockerfile.goscanner --no-cache -t goscanner .
 
 podman build -f=Dockerfile.download_upload -t goscanner-file-manager .
